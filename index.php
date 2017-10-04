d720d5cf9aa002c9245ae227fc9a0d40a4734aee
matt-shade-nbcuni <matt.shade@nbcuni.com>
2017-10-03 21:59:24 -0400

    adding updates

--- index.php
+++ index.php
@@ -1,17 +1,72 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file.
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Listify
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+global $style;
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+$blog_style = get_theme_mod( 'content-blog-style', 'default' );
+$style = 'grid-standard' == $blog_style ? 'standard' : 'cover';
+$sidebar = 'none' != esc_attr( listify_theme_mod( 'content-sidebar-position', 'right' ) ) && is_active_sidebar( 'widget-area-sidebar-1' );
+
+get_header(); ?>
+
+	<?php if ( is_home() ) : ?>
+	<div <?php echo apply_filters( 'listify_cover', 'page-cover', array(
+		'size' => 'full',
+	) ); ?>>
+		<h1 class="page-title cover-wrapper"><?php echo get_option( 'page_for_posts' ) ? get_the_title( get_option( 'page_for_posts' ) ) :  _x( 'Blog', 'blog page title', 'listify' ); ?></h1>
+	</div>
+	<?php endif; ?>
+
+	<div id="primary" class="container">
+		<div class="row content-area">
+
+			<?php if ( 'left' == esc_attr( listify_theme_mod( 'content-sidebar-position', 'right' ) ) ) : ?>
+				<?php get_sidebar(); ?>
+			<?php endif; ?>
+
+			<main id="main" class="site-main col-xs-12 <?php if ( $sidebar ) : ?>col-sm-7 col-md-8<?php endif; ?>" role="main">
+
+				<?php if ( 'default' != $blog_style ) : ?>
+				<div class="blog-archive blog-archive--grid <?php if ( $sidebar ): ?>blog-archive--has-sidebar<?php endif; ?>" data-columns>
+					<?php add_filter( 'excerpt_length', 'listify_short_excerpt_length' ); ?>
+				<?php endif; ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+		
+					<?php
+					if ( 'default' == $blog_style ) :
+						get_template_part( 'content' );
+						else :
+							get_template_part( 'content', 'recent-posts' );
+						endif;
+					?>
+
+				<?php endwhile; ?>
+
+				<?php if ( 'default' != $blog_style ) : ?>
+					<?php remove_filter( 'excerpt_length', 'listify_short_excerpt_length' ); ?>
+					</div>
+				<?php endif; ?>
+
+				<?php get_template_part( 'content', 'pagination' ); ?>
+
+			</main>
+
+			<?php if ( 'right' == esc_attr( get_theme_mod( 'content-sidebar-position', 'right' ) ) ) : ?>
+				<?php get_sidebar(); ?>
+			<?php endif; ?>
+
+		</div>
+	</div>
+
+<?php get_footer(); ?>
